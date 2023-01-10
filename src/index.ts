838322d8b68d8687470315958247b690b9c1905c
dgcezar <diego.cezar13@gmail.com>
2023-01-10 13:11:22 -0300

    métodos GET e DELETE implementados

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,26 @@ app.get("/ping", (req: Request, res: Response) => {
 app.get("/accounts", (req: Request, res: Response) => {
     res.send(accounts)
 })
+
+app.get("/accounts/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+    const result = accounts.find((account) => {
+        return account.id === id
+    })
+        res.status(200).send(result)
+})
+
+app.delete("/accounts/:id", (req: Request, res: Response) => {
+    const id = req.params.id as string
+
+    const accountIndex = accounts.findIndex((account) => {
+        return account.id === id
+    })
+    console.log("Index: ", accountIndex)
+    if(accountIndex>=0) {
+        accounts.splice(accountIndex, 1)
+        res.status(200).send("Item deletado com sucesso!")
+    } else {
+        res.status(404).send("item não encontrado!")
+    }
+})
